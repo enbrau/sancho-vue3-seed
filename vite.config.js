308a0588131c883aa370a9e911d81191afd5ebe5
Enbrau <admin@enbrau.com>
2021-08-28 11:14:33 +0800

    fix: mock and online mode doesn't work problem

--- vite.config.js
+++ vite.config.js
@@ -10,23 +10,14 @@ const mode = process.env.NODE_ENV
 
 export default defineConfig(() => {
   require('dotenv').config({ path: `./.env.${mode}` });
-  return {
+  const config = {
     plugins: [
       vue(),
       // svg icon support
       viteSvgIcons({
         iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
         symbolId: 'icon-[dir]-[name]',
-      }),
-      // https://github.com/anncwb/vite-plugin-mock/
-      viteMockServe({
-        mockPath: 'mock',
-        supportTs: true,
-        watchFiles: true,
-        ignoreFiles: [],
-        prodEnabled: false,
-        logger: true
-    })
+      })
     ],
     resolve: {
       alias: {
@@ -37,20 +28,42 @@ export default defineConfig(() => {
       host: '0.0.0.0',
       port,
       strictPort: false,
+      // https://github.com/http-party/node-http-proxy#options
       proxy: {
-        [process.env.VITE_BASE_URL]: {
-          target: process.env.VITE_BASE_URL === '/mock' ? `http://127.0.0.1:${port}/mock` : `${process.env.VITE_HOST}${process.env.VITE_BASE_URL}`,
+        '/mock': {
+          target: `http://127.0.0.1:${port}`,
           changeOrigin: true,
           ws: true,
           secure: false,
           logLevel: 'debug',
-          pathRewrite: {
-            ['^' + process.env.VITE_BASE_URL]: ''
-          }
+          rewrite: path => path.replace(/\/mock/, '')
+        },
+        '/online': {
+          target: `${process.env.VITE_HOST}`,
+          changeOrigin: true,
+          ws: true,
+          secure: false,
+          logLevel: 'debug',
+          rewrite: path => path.replace(/\/online/, '')
         }
       },
       // this might cause trouble when you are coding on code server!
       // open: '/index.html',
     }
   }
+
+  if (mode === 'development') {
+    config.plugins.push(
+      // https://github.com/anncwb/vite-plugin-mock/
+      viteMockServe({
+        mockPath: 'mock',
+        supportTs: true,
+        watchFiles: true,
+        ignoreFiles: [],
+        prodEnabled: false,
+        logger: true
+      }))
+  }
+
+  return config
 })
